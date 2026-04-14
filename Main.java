c9936bdefd00b7cf486983387e2c73a9d6e99a8e
Ana Julia <131393446+AnaJuliaSFernandes@users.noreply.github.com>
2026-04-13 21:42:23 -0300

    Versão 3

--- Main.java
+++ Main.java
@@ -1,96 +1,131 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 
-class Produto {
-    String nome;
-    double preco;
+class Cliente {
+    private String nome;
 
-    //construtor
-    Produto(String nome, double preco){
+    //construtor / get e set encapisulamento
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+}
+
+class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto (String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
     }
-}
 
-class ProdutoFisico extends Produto {
-    double peso;
-
-    ProdutoFisico(String nome, double preco, double peso){
-        //super chama o contrutor da classe pai
-        super(nome, preco);
-        this.peso = peso;
+    public String getNome() {
+        return nome;
     }
-}
 
-class ProdutoDigital extends Produto {
-    double tamanhoArq;
-
-    ProdutoDigital(String nome, double preco, double tamanhoArq){
-        super(nome, preco);
-        this.tamanhoArq = tamanhoArq;
+    public double getPreco() {
+        return preco;
+    }
+    //this : referenciacia a veriavel declarada lá em cima 
+    public void aplicarDesconto(double percentual) {
+        this.preco = this.preco - (this.preco * percentual / 100);
     }
 }
 
 class Carrinho {
-    ArrayList<Produto>produtos = new ArrayList<>();
+    private ArrayList<Produto>produtos;
 
-    void adicionarProduto(Produto produto){
-        produtos.add(produto);
+    //objeto
+    public Carrinho() {
+        this.produtos = new ArrayList<>();
     }
 
-    void listaProdutos(){
-        System.out.println("Produtos no carrinho: \n");
-
-        for(Produto p: produtos){
-            System.out.println(p.nome + "- R$ " +p.preco);
-
-            //verificando o tipo do produto
-            if (p instanceof ProdutoFisico) {
-                ProdutoFisico pf = (ProdutoFisico) p;
-                System.out.println("Peso: " + pf.peso + " kg");
-            }
-
-            if (p instanceof ProdutoDigital) {
-                ProdutoDigital pd = (ProdutoDigital) p;
-                System.out.println("Tamanho do arquivo: " + pd.tamanhoArq + " MB \n");
-            }
+    public void adicionarProduto(Produto produto) {
+        if (produto.getPreco() > 0) {
+             produtos.add(produto);
+        }else {
+            System.out.println("Produto invalido não adicionado!");
         }
     }
 
-    void calcularTotal(){
+    public void removerProduto(Produto produto) {
+        produtos.remove(produto);
+    }
+
+    public double calcularTotal() {
         double total = 0;
-
-        for(Produto p : produtos){
-            total += p.preco;
+        for (Produto p: produtos) {
+            total += p.getPreco();
         }
-
-        System.out.println("Total de compra R$ " + total);
+        return total;
     }
 
+    public void mostrarProdutos(){
+        
+        //verifica se está vazio
+        if (produtos.isEmpty()) {
+            System.out.println("Carrinho vazio");
+        }
+        for (Produto p : produtos) {
+            System.out.println(p.getNome() + " - R$" + p.getPreco());
+        }
+    }
+
+    public ArrayList<Produto>getProdutos() {
+        return produtos;
+    }
 }
 
-/*class Cliente extends Produto {
-    public Cliente(String nome){
-        this.nome = nome;
+class Pedido {
+    private Cliente cliente;
+    private Carrinho carrinho;
+
+    public Pedido(Cliente cliente, Carrinho carrinho) {
+        this.cliente = cliente;
+        this.carrinho = carrinho;
     }
-}*/
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Carrinho getCarrinho() {
+        return carrinho;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        ProdutoFisico p1 = new ProdutoFisico("Notebook", 5000,2.5);
-        ProdutoDigital p2 = new ProdutoDigital("Curso de Java", 600,1500);
-        
-
+        //cliente
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scanner.next();
+        Cliente cliente = new Cliente(nomeCliente);
+        //Produtos
+        Produto p1 = new Produto("Notebook", 3000);
+        Produto p2 = new Produto("Mouse", 100);
+        Produto p3 = new Produto("Teclado", 200);
+        //Aplicando desconto
+        p1.aplicarDesconto(10);
+        //carrinho
         Carrinho carrinho = new Carrinho();
-
-        /*Cliente user1 = new Cliente("Marcelo");
-        System.out.println(user1.nome + " o seu pedido: ");*/
-
-        carrinho.adicionarProduto(p1);
-        carrinho.adicionarProduto(p2);
-
-        carrinho.listaProdutos();
-        carrinho.calcularTotal();
+        //Menu produtos
+        boolean continuarComprando = true;
+        while (continuarComprando) {
+            System.out.println("\n===========Menu Produtos==========");
+            System.out.println("\n===========Menu Produtos==========");
+            System.out.println("\n===========Menu Produtos==========");
+            System.out.println("\n===========Menu Produtos==========");
+            System.out.println("\n===========Menu Produtos==========");
+            System.out.println("\n===========Menu Produtos==========");
+            System.out.println("\n===========Menu Produtos==========");
+            System.out.println("\n===========Menu Produtos==========");
+        }
     }
     
 }
